45d90c90f7c32d301d5db7157d768482c3690dda
NourhanAymanElstohy <nour7anelsto7y@gmail.com>
2021-04-09 18:56:32 +0200

    sort by rating and datetime

--- resources/js/components/DailyNews.vue
+++ resources/js/components/DailyNews.vue
@@ -1,22 +1,31 @@
 <template>
     <div class="container">
-        <div class="row mb-3">
-            <h2 class="text-center">Daily News</h2>
+        <div class="row mb-4 mt-2">
+            <div class="col-3">
+                 <h2 >Daily News</h2>
+            </div>
+            <div class="col-6"></div>
+            <div class="col-3">
+                <b-dropdown id="dropdown-1" text="Sort By" class="">
+                    <b-dropdown-item @click="sortNews('rating')">Rating {{ this.sortRate == 'asc' ? 'Ascending' : 'Descending'}}</b-dropdown-item>
+                    <b-dropdown-item @click="sortNews('datetime')">Date Time {{ this.sortDate == 'asc' ? 'Ascending' : 'Descending'}}</b-dropdown-item>
+                </b-dropdown>
+            </div>
         </div>
 
-        <div  v-for="agency in agencies" :key="agency.__id">
-            <div class="row mb-1">
+        <div v-for="agency in agencies" :key="agency.__id">
+            <div class="mb-2">
                 <h5 class="text-danger">Agency: <strong class="text-dark">{{ agency.name }}</strong></h5>
             </div>
             <div class="row">
                 <div v-for="(news,i) in agency.latest_news" :key="i" >
                     <b-card
                         :title="news.title"
-                        img-src="https://picsum.photos/200/100/?image=20"
+                        img-src="https://picsum.photos/300/150/?image=20"
                         img-alt="Image"
                         img-top
-                        tag="article"
-                        style="max-width: 21rem;"
+                        tag="div"
+                        style="max-width: 20rem;"
                         class="mb-2 ml-3"
                     >
                         <b-card-text>
@@ -36,7 +45,9 @@
 export default {
     data() {
         return {
-            agencies: []
+            agencies: [],
+            sortRate: 'asc',
+            sortDate: 'asc',
         }
     },
     created() {
@@ -47,5 +58,37 @@ export default {
                 console.log(this.agencies);
             });
     },
+    methods: {
+        sortNews(sortBy){
+            if(sortBy == 'rating'){
+                this.agencies.forEach(agency => {
+                    agency.latest_news.sort((a,b) => {
+                        if(this.sortRate == 'asc') {
+                            return parseFloat(a.rating) - parseFloat(b.rating);
+                        }
+                        else if(this.sortRate == 'desc') {
+                            return parseFloat(b.rating) - parseFloat(a.rating);
+                        }
+                    })
+                });
+                if(this.sortRate == 'asc') this.sortRate = 'desc';
+                else this.sortRate = 'asc';
+                
+            }else if(sortBy == 'datetime'){
+                this.agencies.forEach(agency => {
+                    agency.latest_news.sort((a,b) => { 
+                        if(this.sortDate == 'asc'){
+                            return new Date(a.datetime) - new Date(b.datetime);
+                        }else if(this.sortDate == 'desc'){
+                            return new Date(b.datetime) - new Date(a.datetime);
+                        }
+                        
+                    })
+                });
+                if(this.sortDate == 'asc') this.sortDate = 'desc';
+                else this.sortDate = 'asc';
+            }
+        }
+    }
 }
 </script>
